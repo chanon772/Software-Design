3de90c6600c030b4dcfc6892d8817e358c36cf0f
chaleelaleela <aiyah.bt@kkumail.com>
2021-10-27 17:15:33 +0700

    Edit BranchController.

--- demo/src/main/java/com/example/demo/controller/BranchController.java
+++ demo/src/main/java/com/example/demo/controller/BranchController.java
@@ -8,8 +8,11 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.demo.model.BranchRepository;
+
+import java.util.List;
 import com.example.demo.model.Branch;
 
 
@@ -20,7 +23,14 @@ public class BranchController {
 	@Autowired
 	private BranchRepository branchRepository;
 	
-	@GetMapping("/delete/{id}")
+	@RequestMapping("/branch/all")
+	public String allBranches(Model model) {
+		List<Branch> branches = (List<Branch>) branchRepository.findAll();
+		model.addAttribute("branches", branches);
+		return "/branch/all";
+	}
+	
+	@GetMapping("/branch/delete/{id}")
 	public String deleteBranch(@PathVariable("id") long id, Model model) {
 		Branch branch = branchRepository.findById((int) id)
 			.orElseThrow(() -> new IllegalArgumentException("Invalid branch Id:" + id));
@@ -28,6 +38,20 @@ public class BranchController {
 		branchRepository.delete(branch);
 		return "redirect:/branch/all";
 	}
+	
+	
+	@PostMapping("/branch/update/{id}")
+	  public String updateUser(@PathVariable("id") Integer id, @Validated  Branch branch, 
+	    BindingResult result, Model model) {
+	     
+		if (result.hasErrors()) {
+			branch.setId(id);
+	          return "/branch/edit";
+	      }
+	          
+		branchRepository.save(branch);
+		return "redirect:/branch/all";
+	  }
 	
 	@GetMapping("/branch/edit/{id}")
 	public String showUpdateForm(@PathVariable("id") int id, Model model) {
@@ -51,7 +75,7 @@ public class BranchController {
 	 @GetMapping("/branch/add")
 	  public String showBranchForm(Model model) {
 		 Branch branch = new Branch();
-	      model.addAttribute("emp", branch);
+	      model.addAttribute("branch", branch);
 	      
 	      return "branch/add";
 	  }
--- demo/src/main/java/com/example/demo/model/Branch.java
+++ demo/src/main/java/com/example/demo/model/Branch.java
@@ -16,55 +16,50 @@ public class Branch {
 	@Column(columnDefinition = "MEDIUMINT NOT NULL AUTO_INCREMENT")
 	private int id;	
 	private String name;
-	private String phonenumber;
+	private String phoneNumber;
 	private String email;
 	
 	public Branch() {};
-	public Branch(String name, String phonenumber, String email) {
+	public Branch(String name, String phoneNumber, String email) {
 		super();
 		this.name = name;
-		this.phonenumber = phonenumber;
+		this.phoneNumber = phoneNumber;
 		this.email = email;
 	}
 	
 	
-
+	
 	@Override
 	public String toString() {
-		return "Branch [id=" + id + ", name=" + name + ", phonenumber=" + phonenumber + ", email=" + email + "]";
+		return "Branch [id=" + id + ", name=" + name + ", phoneNumber=" + phoneNumber + ", email=" + email + "]";
 	}
 	
 	public int getId() {
 		return id;
 	}
-
 	public void setId(int id) {
 		this.id = id;
 	}
-
 	public String getName() {
 		return name;
 	}
-
 	public void setName(String name) {
 		this.name = name;
 	}
-
-	public String getPhonenumber() {
-		return phonenumber;
+	public String getPhoneNumber() {
+		return phoneNumber;
 	}
-
-	public void setPhonenumber(String phonenumber) {
-		this.phonenumber = phonenumber;
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
 	}
-
 	public String getEmail() {
 		return email;
 	}
-
 	public void setEmail(String email) {
 		this.email = email;
 	}
+	
+	
 
 	
 	
