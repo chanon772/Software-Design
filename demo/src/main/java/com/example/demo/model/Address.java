9df411a4955e23f5596bc4d4005be3fada5cd80e
chaleelaleela <aiyah.bt@kkumail.com>
2021-10-27 14:14:24 +0700

    Edit address, branch

--- demo/src/main/java/com/example/demo/model/Address.java
+++ demo/src/main/java/com/example/demo/model/Address.java
@@ -1,10 +1,19 @@
 package com.example.demo.model;
 
-@Entity
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name="address")
 public class Address {
 	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
 	private int	id;
+	
 	private String	houseNo;
 	private String	streeet;
 	private String	alley;
@@ -12,6 +21,72 @@ public class Address {
 	private String	subDistrict;
 	private String	province;
 	private String	postalCode;
+	
+	
+	
+	public Address(String houseNo, String streeet, String alley, String villageNo, String subDistrict, String province,
+			String postalCode) {
+		super();
+		this.houseNo = houseNo;
+		this.streeet = streeet;
+		this.alley = alley;
+		this.villageNo = villageNo;
+		this.subDistrict = subDistrict;
+		this.province = province;
+		this.postalCode = postalCode;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getHouseNo() {
+		return houseNo;
+	}
+	public void setHouseNo(String houseNo) {
+		this.houseNo = houseNo;
+	}
+	public String getStreeet() {
+		return streeet;
+	}
+	public void setStreeet(String streeet) {
+		this.streeet = streeet;
+	}
+	public String getAlley() {
+		return alley;
+	}
+	public void setAlley(String alley) {
+		this.alley = alley;
+	}
+	public String getVillageNo() {
+		return villageNo;
+	}
+	public void setVillageNo(String villageNo) {
+		this.villageNo = villageNo;
+	}
+	public String getSubDistrict() {
+		return subDistrict;
+	}
+	public void setSubDistrict(String subDistrict) {
+		this.subDistrict = subDistrict;
+	}
+	public String getProvince() {
+		return province;
+	}
+	public void setProvince(String province) {
+		this.province = province;
+	}
+	public String getPostalCode() {
+		return postalCode;
+	}
+	public void setPostalCode(String postalCode) {
+		this.postalCode = postalCode;
+	}
+	
+	
+	
 	
 	
 }
--- demo/src/main/java/com/example/demo/model/Branch.java
+++ demo/src/main/java/com/example/demo/model/Branch.java
@@ -1,8 +1,62 @@
 package com.example.demo.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="branch")
 public class Branch {
 	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
 	private int id;	
 	private String name;
+	private String phonenumber;
+	private String email;
+	
+	public Branch(String name, String phonenumber, String email) {
+		super();
+		this.name = name;
+		this.phonenumber = phonenumber;
+		this.email = email;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPhonenumber() {
+		return phonenumber;
+	}
+
+	public void setPhonenumber(String phonenumber) {
+		this.phonenumber = phonenumber;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	
+	
 	
 }
