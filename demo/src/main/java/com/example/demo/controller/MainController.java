5cbbe1eb1a38bd6b9770e647879067e19fc5ffde
chaleelaleela <aiyah.bt@kkumail.com>
2021-10-29 00:48:45 +0700

    Add index.

--- demo/src/main/java/com/example/demo/controller/MainController.java
+++ demo/src/main/java/com/example/demo/controller/MainController.java
@@ -1,17 +1,27 @@
 package com.example.demo.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.demo.model.Branch;
+import com.example.demo.repository.BranchRepository;
 
 
 
 @Controller
 public class MainController {
+	@Autowired
+	private BranchRepository branchRepository;
 	
 	@RequestMapping("/")
 	public String index(Model model) {
 		
-		return "index";
+		List<Branch> branches = (List<Branch>) branchRepository.findAll();
+		model.addAttribute("branches", branches);
+		return "/branch/all";
 	}
 }
