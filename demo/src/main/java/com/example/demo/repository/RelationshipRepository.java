c68b0a801e7c208a5b3544c958a3d88ea6b068a1
chaleelaleela <aiyah.bt@kkumail.com>
2021-10-28 21:46:58 +0700

    Add "add/remove an employee to a branch."

--- demo/src/main/java/com/example/demo/repository/RelationshipRepository.java
+++ demo/src/main/java/com/example/demo/repository/RelationshipRepository.java
@@ -3,8 +3,13 @@ package com.example.demo.repository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.data.repository.CrudRepositoryExtensionsKt;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
+
+import com.example.demo.model.Employee;
+
+import java.util.List;
 
 @Repository
 public class RelationshipRepository  {
@@ -24,6 +29,15 @@ public class RelationshipRepository  {
         String sql_str  = "SELECT address_id from  branch_address WHERE branch_id = ?";
 
         return jdbcTemplate.queryForObject(sql_str,  new Object[]{id}, Integer.class);
+    }
+    
+    public List<Employee> findAllAvailableEmployee(){
+    	
+    	String sql_str = "SELECT * from employee where id not in ("
+    			+ "SELECT employee_id from branch_employee)";
+    	
+    	return jdbcTemplate.query(sql_str, new BeanPropertyRowMapper(Employee.class));
+    	
     }
 
 }
